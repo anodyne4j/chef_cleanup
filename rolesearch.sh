7663e7d2295acdb3600bf5b2fde5a76e66c7dbaa
jba <jba@bitcasa.com>
2016-02-05 13:42:13 -0800

    sort the list of dead nodes

--- rolesearch.sh
+++ rolesearch.sh
@@ -4,8 +4,10 @@ rm ./roleswithhosts.txt
 rm ./rolesnohosts.txt
 rm ./allroles.txt
 rm ./deadnodes.txt
+rm ./unsortednodes.txt
 
 knife role list > allroles.txt
+
 for role in `tr '\n' ' ' < allroles.txt`
 do 
   HOSTS=`cat ./empty.txt`
@@ -21,8 +23,10 @@ do
     do
       HOSTIP=`resolveip -s $host`
       if [ "$HOSTIP" = "$EMPTY" ]; then
-        echo $host >> ./deadnodes.txt
+        echo $host >> ./unsortednodes.txt
       fi
     done
   fi
 done
+
+cat ./unsortednodes.txt | sort > ./deadnodes.txt
